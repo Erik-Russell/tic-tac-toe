7f02e0f7449cb6f3b13505a76c70367d9812bb6a
Erik Russell <erik.z.russell@gmail.com>
2024-09-03 11:10:05 -0700

    Replaced individual methods with new Grid class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,29 +1,22 @@
 # frozen_string_literal: true
 
-require_relative 'lib/create_new_grid'
-require_relative 'lib/display_grid'
-require_relative 'lib/map_indexes'
-require_relative 'lib/update_grid'
+require_relative 'lib/grid'
 
-# declare variables
-variable_names = %w[a1 a2 a3 b1 b2 b3 c1 c2 c3]
+# new game
+game_board = Grid.new
 
-# create a blank grid
-grid = create_new_grid
-
-# create index map
-index_map = map_indexes
-
-# Map grid elements to variable names using a hash
-grid_hash = {}
-grid.flatten.map.with_index do |_, index|
-  grid_hash[variable_names[index]] = ' '
-end
+# display new board
+game_board.display
 
 # EXAMPLE FILLER ----------------------
-grid_hash['a2'] = 'X'
+game_board.grid[1][1] = 'X'
+game_board.grid[0][2] = 'O'
+game_board.grid[0][1] = 'X'
 
-# Update grid from hash
-update_grid(grid, grid_hash, index_map)
+# display board with moves
+game_board.display
 
-display_grid(grid)
+# new game
+game_board.reset
+
+game_board.display
