68547363bda1f7e049024e8d82603984e4da7349
Erik Russell <erik.z.russell@gmail.com>
2024-10-02 09:31:13 -0700

    test Game#valid_input?

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -17,7 +17,41 @@ describe Game do
 
   describe '#player_input'
 
-  describe '#valid_input?'
+  describe '#valid_input?' do # rubocop:disable Metrics/BlockLength
+    context 'when input is valid' do
+      it 'returns true' do
+        input = %w[1 1]
+        expect(game.valid_input?(input)).to be true
+      end
+    end
+    context 'when input is too long' do
+      it 'return false' do
+        input = %w[1 1 1]
+        expect(game.valid_input?(input)).to be false
+      end
+    end
+
+    context 'when input is too short' do
+      it 'return false' do
+        input = %w[1]
+        expect(game.valid_input?(input)).to be false
+      end
+    end
+
+    context 'when input contains a number that is not 1, 2, or 3' do
+      it 'return false' do
+        input = %w[1 6]
+        expect(game.valid_input?(input)).to be false
+      end
+    end
+
+    context 'when input contains a non integer' do
+      it 'return false' do
+        input = %w[a 6]
+        expect(game.valid_input?(input)).to be false
+      end
+    end
+  end
 
   describe '#play_turn'
 end
